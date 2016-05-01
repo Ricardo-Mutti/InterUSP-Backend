0dbef452c6469ab351b8e31ee3a65ce395b37ae2
Rafael Camargos Santos <rafael.appsimples@gmail.com>
2016-05-01 03:30:15 -0300

    controller get locais

---
+++ modules/local/locais-controller.js
@@ -0,0 +1,14 @@
+module.exports = function (schema){
+
+	var Local = schema.local;
+
+	return{
+		getLocais: function(req, res){
+			Local.find(function(err, docs){
+		    	if (err) throw err;
+
+		    	return res.json({success: true, message: "Locais encontrados", response:{locais:docs}});
+  			});
+		}
+	}
+}
