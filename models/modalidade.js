b8417750d5dc2f951a17c2dcba7c2b46f7e16e15
Rafael Camargos Santos <rafael.appsimples@gmail.com>
2016-05-09 12:32:47 -0300

    refactor locais

--- models/modalidade.js
+++ models/modalidade.js
@@ -4,9 +4,9 @@ module.exports = function(mongoose) {
   var modalidadeSchema = new Schema({
   	id: Number,
     nome: String,
-    pontuacao_total: [{"faculdade": Number, "pontuacao": Number, "posicao": Number}],
-    pontuacao_max: [{"faculdade": Number, "pontuacao": Number}],
-    pontuacao_min: [{"faculdade": Number, "pontuacao": Number}],
+    pontuacao_total: [{faculdade: Number, pontuacao: Number, posicao: Number}],
+    pontuacao_max: [{faculdade: Number, pontuacao: Number}],
+    pontuacao_min: [{faculdade: Number, pontuacao: Number}],
     is_chaveamento: Boolean
   });
 
